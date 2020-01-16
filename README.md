f3d4d6c9d023c39d2b5c79178c7e20373dc812ca
Clemens Kaserer <clemens.kaserer@gmail.com>
2020-01-16 15:44:17 +0100

    set timezone, keyboard, sys lang, add desktop

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# packer vms
+![GitHub](https://img.shields.io/github/license/gepardec/packer-vms)
+![Maintenance](https://img.shields.io/maintenance/yes/2020)
 
-based on https://github.com/mrlesmithjr/packer-templates
+<p align="right">
+<img alt="gepardec" width=100px src="https://github.com/Gepardec/packer-vms/raw/master/.images/gepardec.png">
+</p>
+<br>
+<br>
+<p align="center">
+<img alt="logos" width=400px src="https://github.com/Gepardec/packer-vms/raw/master/.images/packer.png">
+</p>
+<br>
+
+# Building reproducable VMs with packer
+
+<!-- based on https://github.com/mrlesmithjr/packer-templates --> 
--- troubleshooting-workshop/Fedora/31/scripts/cleanup.sh
+++ troubleshooting-workshop/Fedora/31/scripts/cleanup.sh
@@ -7,31 +7,12 @@ if [ -f /etc/os-release ]; then
   # shellcheck disable=SC1091
   source /etc/os-release
   id=$ID
-
 elif [ -f /etc/redhat-release ]; then
   id="$(awk '{ print tolower($1) }' /etc/redhat-release | sed 's/"//g')"
 fi
 
-if [[ $id == "arch" ]]; then
-  /usr/bin/yes | sudo /usr/bin/pacman -Scc
-
-elif [[ $id == "centos" || $id == "ol" ]]; then
-  sudo yum clean all
-  sudo rm -rf /var/cache/yum
-
-elif [[ $id == "debian" || $id == "elementary" || $id == "linuxmint" || $id == "ubuntu" ]]; then
-  sudo apt-get clean
-
-elif [[ $id == "fedora" ]]; then
+if [[ $id == "fedora" ]]; then
   sudo dnf clean all
-
-elif [[ $id == "opensuse" || $id == "opensuse-leap" ]]; then
-  if [ -f /var/lib/misc/random-seed ]; then
-    rm /var/lib/misc/random-seed
-  fi
-  if [ -f /var/lib/systemd/random-seed ]; then
-    rm /var/lib/systemd/random-seed
-  fi
 fi
 
 # Stop rsyslog service
